9aea8b4153d92b142ffc3941f773e8c4334205c7
Rodion <rodionhatskevich@gmail.com>
2024-11-19 23:38:02 +0100

    create models and dtos

---
+++ eCommerce.OrderApiSolution/OrderApi.Application/DTOs/Conversions/OrderConversion.cs
@@ -0,0 +1,49 @@
+﻿using OrderApi.Application.DTO;
+using OrderApi.Domain.Entities;
+
+namespace OrderApi.Application.DTOs.Conversions
+{
+    public static class OrderConversion
+    {
+        public static Order ToEntity(OrderDTO order) => new()
+        {
+            Id = order.Id,
+            ClientId = order.ClientId,
+            ProductId = order.ProductId,
+            OrderedDate = order.OrderedDate,
+            PurchaseQuantity = order.PurchaseQuantity
+        };
+
+        public static (OrderDTO?, IEnumerable<OrderDTO>?) FromEntity(Order? order, IEnumerable<Order>? orders)
+        {
+            //return single
+            if (order is not null || orders is null)
+            {
+                var singleOrder = new OrderDTO(
+                    order!.Id,
+                    order.ClientId,
+                    order.ProductId,
+                    order.PurchaseQuantity,
+                    order.OrderedDate);
+
+                return (singleOrder, null);
+            }
+
+            //return list
+            if (order is null || orders is not null)
+            {
+                var _orders = orders!.Select(o =>
+                new OrderDTO(
+                    order!.Id,
+                    order.ClientId,
+                    order.ProductId,
+                    order.PurchaseQuantity,
+                    order.OrderedDate));
+
+                return (null, _orders);
+            }
+
+            return (null, null);
+        }
+    }
+}
